c1e307a3c9f8a28f4e3fd36e9b1000943db6d4af
XenXorX <it58070024@office365.it.kmitl.ac.th>
2017-11-16 03:43:37 +0700

    Add testcase

--- app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
@@ -30,4 +30,13 @@ public class MainActivityTest {
         onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("XenXorX@gmail.com"));
         onView(withId(R.id.saveButton)).perform(scrollTo(), click());
     }
+
+    @Test
+    public void saveEmptyName() {
+        closeSoftKeyboard();
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText(""));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("XenXorX@gmail.com"));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+    }
 }
